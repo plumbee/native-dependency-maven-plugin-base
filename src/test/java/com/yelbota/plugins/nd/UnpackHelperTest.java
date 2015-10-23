ad8d47005f98e220d67fd038b20c1c13bc9bd522
Pablo Varela <pablo.varela@plumbee.co.uk>
2015-10-23 15:27:53 +0100

    Removed code duplication in UnpackHelperTest

--- src/test/java/com/yelbota/plugins/nd/UnpackHelperTest.java
+++ src/test/java/com/yelbota/plugins/nd/UnpackHelperTest.java
@@ -16,24 +16,20 @@
 package com.yelbota.plugins.nd;
 
 import com.yelbota.plugins.nd.utils.UnpackMethod;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.DefaultArtifact;
-import org.apache.maven.artifact.handler.DefaultArtifactHandler;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugin.testing.stubs.ArtifactStub;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.attribute.FileAttribute;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.StringContains.containsString;
@@ -42,62 +38,59 @@ import static org.testng.Assert.*;
 
 public class UnpackHelperTest {
 
+    private static final Log NO_LOGGER = null;
+
+    private Map<String, UnpackMethod> mockedUnpackMethods;
+    private UnpackMethod mockedUnpackedMethod = mock(UnpackMethod.class);
+
     private class UnpackHelperWithExtraLogging extends UnpackHelper {
         private boolean logAlreadyUnpackedCalled = false;
 
         @Override
         protected void logAlreadyUnpacked() {
-           this.logAlreadyUnpackedCalled = true;
+            this.logAlreadyUnpackedCalled = true;
         }
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        this.mockedUnpackedMethod = mock(UnpackMethod.class);
+        Map<String, UnpackMethod> unpackMethodMap = new HashMap<String, UnpackMethod>();
+        unpackMethodMap.put("mock-type", this.mockedUnpackedMethod);
+        this.mockedUnpackMethods = Collections.unmodifiableMap(unpackMethodMap);
     }
 
     @Test
     public void testUnpackWhenFileTypeIsUnknown() throws Exception {
-        UnpackMethod mockedUnpackMethod = mock(UnpackMethod.class);
-
-        Map<String, UnpackMethod> unpackMethods = new HashMap<String, UnpackMethod>();
-        unpackMethods.put("mock-type", mockedUnpackMethod);
-
         UnpackHelper unpackHelper = new UnpackHelper();
         File invalidDirectory = Files.createTempFile("unpack-help-test", ".mock").toFile();
         try {
-            ArtifactStub artifactStub = new ArtifactStub();
-            artifactStub.setType("unknown-type");
-            unpackHelper.unpack(invalidDirectory, artifactStub, unpackMethods, null);
+            ArtifactStub artifact = createArtifactStub();
+            artifact.setType("unknown-type");
+            unpackHelper.unpack(invalidDirectory, artifact, this.mockedUnpackMethods, NO_LOGGER);
             fail("should fail unpack call if the file type is invalid");
         } catch (MojoFailureException expected) {
             assertThat(expected.getMessage(), containsString("unknown type"));
         }
-        verifyZeroInteractions(mockedUnpackMethod);
+        verifyZeroInteractions(this.mockedUnpackedMethod);
     }
 
     @Test
     public void testUnpackWhenDirectoryIsInvalid() throws Exception {
-        UnpackMethod mockedUnpackMethod = mock(UnpackMethod.class);
-
-        Map<String, UnpackMethod> unpackMethods = new HashMap<String, UnpackMethod>();
-        unpackMethods.put("mock-type", mockedUnpackMethod);
-
         UnpackHelper unpackHelper = new UnpackHelper();
         File invalidDirectory = Files.createTempFile("unpack-help-test", ".mock").toFile();
         try {
-            ArtifactStub artifactStub = new ArtifactStub();
-            artifactStub.setType("mock-type");
-            unpackHelper.unpack(invalidDirectory, artifactStub, unpackMethods, null);
+            ArtifactStub artifact = createArtifactStub();
+            unpackHelper.unpack(invalidDirectory, artifact, this.mockedUnpackMethods, NO_LOGGER);
             fail("should fail unpack call if the directory is invalid");
         } catch (MojoFailureException expected) {
             assertThat(expected.getMessage(), containsString("must be directory"));
         }
-        verifyZeroInteractions(mockedUnpackMethod);
+        verifyZeroInteractions(this.mockedUnpackedMethod);
     }
 
     @Test
-    public void testUnpackWhenCanNotCreateDirectory() throws Exception {
-        UnpackMethod mockedUnpackMethod = mock(UnpackMethod.class);
-
-        Map<String, UnpackMethod> unpackMethods = new HashMap<String, UnpackMethod>();
-        unpackMethods.put("mock-type", mockedUnpackMethod);
-
+    public void testUnpackWhenDirectoryToUnpackCanNotBeCreated() throws Exception {
         UnpackHelper unpackHelper = new UnpackHelper();
         Set<PosixFilePermission> readOnlyPermissions = PosixFilePermissions.fromString("r-xr-xr-x");
         FileAttribute<Set<PosixFilePermission>> fileAttributes = PosixFilePermissions.asFileAttribute(readOnlyPermissions);
@@ -105,33 +98,27 @@ public class UnpackHelperTest {
         File readOnlyRootDirectory = Files.createTempDirectory("unpack-helper-test", fileAttributes).toFile();
         File invalidDirectory = new File(readOnlyRootDirectory, "should-fail-to-create");
         try {
-            ArtifactStub artifactStub = new ArtifactStub();
-            artifactStub.setType("mock-type");
-            unpackHelper.unpack(invalidDirectory, artifactStub, unpackMethods, null);
+            ArtifactStub artifact = createArtifactStub();
+            unpackHelper.unpack(invalidDirectory, artifact, this.mockedUnpackMethods, NO_LOGGER);
             fail("should fail unpack call if the directory is invalid");
         } catch (MojoFailureException expected) {
             assertThat(expected.getMessage(), containsString("could not create directory"));
         }
-        verifyZeroInteractions(mockedUnpackMethod);
+        verifyZeroInteractions(this.mockedUnpackedMethod);
     }
 
     @Test
     public void testUnpackWhenUnpackMethodThrowsException() throws Exception {
         UnpackMethod mockedUnpackMethod = mock(UnpackMethod.class);
         doThrow(new UnpackMethod.UnpackMethodException("something went wrong")).when(mockedUnpackMethod).unpack(any(File.class), any(File.class), any(Log.class));
-
         Map<String, UnpackMethod> unpackMethods = new HashMap<String, UnpackMethod>();
         unpackMethods.put("mock-type", mockedUnpackMethod);
 
         File directory = new File(System.getProperty("java.io.tmpdir"), "unpack-helper-test-" + UUID.randomUUID());
-
-        Artifact artifact = new DefaultArtifact("groupId", "artifactId", "version", "scope", "mock-type", "classifier", new DefaultArtifactHandler());
-        artifact.setFile(Files.createTempFile("unpack-help-test-artifact", ".mock").toFile());
-
-        Log mockedLog = mock(Log.class);
+        ArtifactStub artifact = createArtifactStub();
         UnpackHelperWithExtraLogging unpackHelper = new UnpackHelperWithExtraLogging();
         try {
-            unpackHelper.unpack(directory, artifact, unpackMethods, mockedLog);
+            unpackHelper.unpack(directory, artifact, unpackMethods, NO_LOGGER);
             fail("should fail unpack call if UnpackMethod fails");
         } catch (UnpackHelper.UnpackHelperException expected) { /* no op */ }
 
@@ -142,44 +129,37 @@ public class UnpackHelperTest {
 
     @Test
     public void testUnpackWhenUnpackDirectoryDoesNotExist() throws Exception {
-        UnpackMethod mockedUnpackMethod = mock(UnpackMethod.class);
-        Map<String, UnpackMethod> unpackMethods = new HashMap<String, UnpackMethod>();
-        unpackMethods.put("mock-type", mockedUnpackMethod);
-
         File directory = new File(System.getProperty("java.io.tmpdir"), "unpack-helper-test-" + UUID.randomUUID());
-
-        Artifact artifact = new DefaultArtifact("groupId", "artifactId", "version", "scope", "mock-type", "classifier", new DefaultArtifactHandler());
-        artifact.setFile(Files.createTempFile("unpack-help-test-artifact", ".mock").toFile());
-
-        Log mockedLog = mock(Log.class);
+        ArtifactStub artifact = createArtifactStub();
         UnpackHelperWithExtraLogging unpackHelper = new UnpackHelperWithExtraLogging();
-        unpackHelper.unpack(directory, artifact, unpackMethods, mockedLog);
+
+        unpackHelper.unpack(directory, artifact, this.mockedUnpackMethods, NO_LOGGER);
         assertFalse(unpackHelper.logAlreadyUnpackedCalled, "the first time unpack is invoked it does not see the artifact as unpacked");
         assertTrue(Files.exists(Paths.get(directory.getAbsolutePath(), UnpackHelper.UNPACKED_COMPLETED_FLAG_FILE)), "flag file is created");
-        verify(mockedUnpackMethod).unpack(any(File.class), any(File.class), any(Log.class));
+        verify(this.mockedUnpackedMethod).unpack(any(File.class), any(File.class), any(Log.class));
     }
 
     @Test
     public void testTwoConsecutiveCallsToUnpackWhenDirectoryAlreadyExists() throws Exception {
-        UnpackMethod mockedUnpackMethod = mock(UnpackMethod.class);
-        Map<String, UnpackMethod> unpackMethods = new HashMap<String, UnpackMethod>();
-        unpackMethods.put("mock-type", mockedUnpackMethod);
-
         File directory = Files.createTempDirectory("unpack-helper-test").toFile();
-
-        Artifact artifact = new DefaultArtifact("groupId", "artifactId", "version", "scope", "mock-type", "classifier", new DefaultArtifactHandler());
-        artifact.setFile(Files.createTempFile("unpack-help-test-artifact", ".mock").toFile());
-
-        Log mockedLog = mock(Log.class);
+        ArtifactStub artifact = createArtifactStub();
         UnpackHelperWithExtraLogging unpackHelper = new UnpackHelperWithExtraLogging();
-        unpackHelper.unpack(directory, artifact, unpackMethods, mockedLog);
+
+        unpackHelper.unpack(directory, artifact, this.mockedUnpackMethods, NO_LOGGER);
         assertFalse(unpackHelper.logAlreadyUnpackedCalled, "the first time unpack is invoked it does not see the artifact as unpacked");
         assertTrue(Files.exists(Paths.get(directory.getAbsolutePath(), UnpackHelper.UNPACKED_COMPLETED_FLAG_FILE)), "flag file is created");
-        verify(mockedUnpackMethod).unpack(any(File.class), any(File.class), any(Log.class));
+        verify(this.mockedUnpackedMethod).unpack(any(File.class), any(File.class), any(Log.class));
 
-        unpackHelper.unpack(directory, artifact, unpackMethods, mockedLog);
+        unpackHelper.unpack(directory, artifact, this.mockedUnpackMethods, NO_LOGGER);
         assertTrue(unpackHelper.logAlreadyUnpackedCalled, "the second time unpack is invoked it sees the artifact as already unpacked");
         assertTrue(Files.exists(Paths.get(directory.getAbsolutePath(), UnpackHelper.UNPACKED_COMPLETED_FLAG_FILE)),  "flag file is not removed");
-        verifyNoMoreInteractions(mockedUnpackMethod);
+        verifyNoMoreInteractions(this.mockedUnpackedMethod);
+    }
+
+    private ArtifactStub createArtifactStub() throws IOException {
+        ArtifactStub artifact = new ArtifactStub();
+        artifact.setType("mock-type");
+        artifact.setFile(Files.createTempFile("unpack-help-test-artifact", ".mock").toFile());
+        return artifact;
     }
 }
